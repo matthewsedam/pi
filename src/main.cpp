9b512a8d0f84058338314a1f9695291c218f0c61
Matthew Sedam <sedammatthew@gmail.com>
2014-03-26 18:21:36 -0400

    Working model; SLOW

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,26 @@
  along with Pi.  If not, see <http://www.gnu.org/licenses/>. */
 
 #include <iostream>
+#include <iomanip>
+#include <string>
 #include <gmp.h>
 #include <gmpxx.h>
 
 int main(int argc, const char *argv[]) {
+    std::cout << std::setprecision(50);
+    mpz_class k = 1;
+    mpf_class pi = mpf_class(0.0, 10485760);
+    mpf_class piIteration = mpf_class(0.0, 10485760);
+    while (true) {
+        if (k % 2 == 1)
+            piIteration = mpf_class(4.0, 10485760);
+        else
+            piIteration = mpf_class(-4.0, 10485760);
+        piIteration = piIteration / mpf_class(mpz_class(2*k) - mpz_class(1), 10485760);
+        pi = pi + piIteration;
+        piIteration = mpf_class(0.0, 10485760);
+        std::cout << pi << std::endl;
+        k = k + 1;
+    }
     return 0;
 }
